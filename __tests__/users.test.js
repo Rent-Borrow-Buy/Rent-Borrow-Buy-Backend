645897fbb07c7c3e800ac1b001bc5793c6e68a39
Andrea Cleland <andreaclelan@gmail.com>
2022-07-12 13:04:21 -0700

    implemented sign in feature with test passing

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -2,10 +2,23 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const UserService = require('../lib/services/UserService');
 
 const mockUser = {
   email: 'testing@example.com',
   password: '123456',
+};
+
+const registerAndLogin = async (userProps = {}) => {
+  const password = userProps.password ?? mockUser.password;
+
+  const agent = request.agent(app);
+
+  const user = await UserService.create({ ...mockUser, ...userProps });
+
+  const { email } = user;
+  await agent.post('/api/v1/users/sessions').send({ email, password });
+  return [agent, user];
 };
 
 describe('users routes', () => {
@@ -18,6 +31,17 @@ describe('users routes', () => {
     expect(res.body).toHaveProperty('email', email);
     expect(res.body).not.toHaveProperty('password');
   });
+
+  it('GET /api/v1/users/me should return current user info', async () => {
+    const [agent, user] = await registerAndLogin();
+    const me = await agent.get('/api/v1/users/me');
+    expect(me.body).toEqual({
+      ...user,
+      exp: expect.any(Number),
+      iat: expect.any(Number),
+    });
+  });
+
   afterAll(() => {
     pool.end();
   });
