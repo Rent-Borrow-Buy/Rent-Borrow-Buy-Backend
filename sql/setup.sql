c3e23237292763087100e7ae9be8625070d14d91
Yovana Pelayo <yovana_pelayo@yahoo.com>
2022-07-12 10:52:18 -0700

    created users table and established route

--- sql/setup.sql
+++ sql/setup.sql
@@ -1,2 +1,13 @@
 -- Use this file to define your SQL tables
 -- The SQL in this file will be executed when you run `npm run setup-db`
+DROP TABLE IF EXISTS users cascade;
+
+
+CREATE TABLE users (
+id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+username VARCHAR,
+email VARCHAR NOT NULL UNIQUE,
+avatar VARCHAR,
+password_hash VARCHAR NOT NULL,
+zipcode INT
+);
