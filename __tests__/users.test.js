d542650bb090c37bdb2e761ab7b754ef5b1dcccf
Andrea Cleland <andreaclelan@gmail.com>
2022-07-12 12:20:55 -0700

    implemented UserService.create method with test passing

--- __tests__/users.test.js
+++ __tests__/users.test.js
@@ -3,25 +3,20 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-
 const mockUser = {
   email: 'testing@example.com',
   password: '123456',
 };
 
-
-
 describe('users routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('Post /api/v1/users should create user', async() => {
+  it('Post /api/v1/users should create user', async () => {
     const res = await request(app).post('/api/v1/users').send(mockUser);
     const { email } = mockUser;
-    expect(res.body).toEqual({
-      id: expect.any(String),
-      email
-    });
+    expect(res.body).toHaveProperty('email', email);
+    expect(res.body).not.toHaveProperty('password');
   });
   afterAll(() => {
     pool.end();
